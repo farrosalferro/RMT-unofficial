6c554838a0093d58c124bc36cc562d94904b527b
farrosalferro <farros240802@gmail.com>
2023-11-29 19:52:46 +0900

    Modified tiny config.

--- configs/_base_/models/rmt/tiny_224.py
+++ configs/_base_/models/rmt/tiny_224.py
@@ -1,13 +1,14 @@
 # model settings
 model = dict(
     type='ImageClassifier',
-    backbone=dict(type='RMT', arch='tiny', img_size=224, drop_path_rate=0.2),
+    backbone=dict(
+        type='RMT', arch='tiny', img_size=224, drop_path_rate=0.2),
     neck=dict(type='GlobalAveragePooling'),
     head=dict(
         type='LinearClsHead',
         num_classes=1000,
         in_channels=96,
-        init_cfg=None,
+        init_cfg=None,  # suppress the default init_cfg of LinearClsHead.
         loss=dict(
             type='LabelSmoothLoss', label_smooth_val=0.1, mode='original'),
         cal_acc=False),

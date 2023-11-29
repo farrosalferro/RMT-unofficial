a727ee8329dd2a39f8581bc84814c01dec506e94
farrosalferro <farros240802@gmail.com>
2023-11-29 20:16:29 +0900

    Modified this config.

--- configs/rmt/rmt-tiny_bs1024_cifar10-224px.py
+++ configs/rmt/rmt-tiny_bs1024_cifar10-224px.py
@@ -8,6 +8,19 @@ model = dict(backbone=dict(drop_path_rate=0.1),
                  dict(type='RandomErasing', erase_prob=0.5)
              ]))
 
+
+# dataset settings
+train_dataloader = dict(batch_size=1024, 
+			dataset=dict(
+				data_root='../../../../beegfs/ImageNet/ilsvrc12/train/'),
+			)
+
+val_dataloader = dict(batch_size=1024,
+			dataset=dict(
+				data_root='../../../../beegfs/ImageNet/ilsvrc12/val/'),
+			)
+
+
 # schedule settings
 optim_wrapper = dict(clip_grad=dict(max_norm=5.0),
                      optimizer=dict(lr=0.001))
